e22b383a3704b8d82108604559cdcbf647721ff1
choi sung ki <florist.sk@gmail.com>
2014-03-18 00:04:45 +0900

    문법파일에서 생성된 파일을 main에서 호출하도록 수정

--- generator/Program.cs
+++ generator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Antlr.Runtime;
+using AntlrTest;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,12 @@ namespace generator
     {
         static void Main(string[] args)
         {
+            const string input = "1+4/2";
+            var lex = new SimpleCalcLexer(new ANTLRStringStream(input));
+            var tokens = new CommonTokenStream(lex);
+            var parser = new SimpleCalcParser(tokens);
+
+            var ret = parser.expr();
         }
     }
 }
